c2f9968f125f8ba0ea36bdc3a8cdd807defad5ab
samshteinman <samshteinman@gmail.com>
2019-09-03 21:46:03 -0400

    Update background.js

--- background.js
+++ background.js
@@ -2,19 +2,19 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab)
 {
 	if (changeInfo.status == 'complete')
 	{
-	chrome.history.getVisits({url: tab.url}, function(results)
+		chrome.history.getVisits({url: tab.url}, function(results)
 		{	
 			if(results.length > 1)
 			{				
 				chrome.tabs.executeScript(tabId, {
-    code: 'var div=document.createElement("div"); document.body.append(div); div.innerText="BEEN HERE BEFORE"; div.style.fontSize = "100px";'
-  });
+    								code: 'var div=document.createElement("div"); document.body.append(div); div.innerText="BEEN HERE BEFORE"; div.style.fontSize = "100px";'
+ 							 });
 			}
 			else
-	{
-		chrome.tabs.executeScript(tabId,{
-code: 'var div=document.createElement("div"); document.body.append(div); div.innerText="NOT BEEN HERE BEFORE"; div.style.fontSize = "100px";'
-  });
+			{
+				chrome.tabs.executeScript(tabId,{
+								code: 'var div=document.createElement("div"); document.body.append(div); div.innerText="NOT BEEN HERE BEFORE"; div.style.fontSize = "100px";'
+ 							 });
 			}
 		});	
 	}
